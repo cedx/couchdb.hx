1c02212c5aa0bddc8e422800ad3b558f78ad3fe4
Cédric Belin <cedric@belin.io>
2023-09-23 14:16:49 +0200

    Enable the unit tests

---
+++ test/couchdb/ServerTest.hx
@@ -0,0 +1,24 @@
+package couchdb;
+
+/** Tests the features of the `Server` class. **/
+@:asserts final class ServerTest {
+
+	/** The server instance. **/
+	final server = new Server("http://localhost:5984");
+
+	/** Creates a new test. **/
+	public function new() {}
+
+	/** Tests the `info` property. **/
+	public function info() {
+		server.info.next(info -> {
+			asserts.assert(info.features.length > 0);
+			asserts.assert(~/[a-z\d]{9}/.match(info.gitSha));
+			asserts.assert(~/[a-z\d]{32}/.match(info.uuid));
+			asserts.assert(info.vendor == "The Apache Software Foundation");
+			asserts.assert(info.version.split(".").shift() == "3");
+		}).handle(asserts.handle);
+
+		return asserts;
+	}
+}
