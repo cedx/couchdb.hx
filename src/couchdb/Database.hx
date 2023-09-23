0760c99df35ef6c4ced383619fa819afcfe67430
Cédric Belin <cedric@belin.io>
2023-09-23 14:16:37 +0200

    Enable the CI

--- src/couchdb/Database.hx
+++ src/couchdb/Database.hx
@@ -1,1 +1,14 @@
 package couchdb;
+
+import tink.Url;
+using StringTools;
+
+/** Provides access to a CouchDB database. **/
+class Database {
+
+	/** The server URL. **/
+	public final url: Url;
+
+	/** Creates a new database. **/
+	public function new(name: String, server: Server) url = server.url.resolve(name);
+}
