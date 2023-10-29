d3547827a779c44314264c632000fd07796aabf9
Cédric Belin <cedric@belin.io>
2023-10-29 21:26:56 +0100

    Add the `Document` class

--- src/couchdb/Document.hx
+++ src/couchdb/Document.hx
@@ -1,1 +1,16 @@
 package couchdb;
+
+using haxe.io.Path;
+
+/** Represents a CouchDB document. **/
+class Document implements Model {
+
+	/** The associated database. **/
+	@:constant var database: Database;
+
+	/** The document key. **/
+	@:constant var key: String;
+
+	/** The database URL. **/
+	@:computed var url: Url = Url.parse(database.url.toString().addTrailingSlash()).resolve(key);
+}
