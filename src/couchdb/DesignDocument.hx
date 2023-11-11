6b2e2c1feb638dcb108b12dc0573c5fc072b50e0
Cédric Belin <cedric@belin.io>
2023-11-11 14:58:53 +0100

    Add the `DesignDocument` class

--- src/couchdb/DesignDocument.hx
+++ src/couchdb/DesignDocument.hx
@@ -1,1 +1,25 @@
 package couchdb;
+
+using haxe.io.Path;
+
+/** Represents a CouchDB design document. **/
+class DesignDocument implements Model {
+
+	/** The associated database. **/
+	@:constant var db: Database;
+
+	/** The document identifier. **/
+	@:computed var id: String = '_design/$key';
+
+	/** The document key. **/
+	@:constant var key: String;
+
+	/** The document URL. **/
+	@:computed var url: Url = Url.parse(db.url.toString().addTrailingSlash()).resolve(id);
+
+	/** The remote API client. **/
+	@:computed private var remote: Remote<RemoteApi> = @:privateAccess db.remote;
+
+	/** Returns an object for performing operations on a view. **/
+	public inline function use(view: String) return new View({design: this, key: view});
+}
